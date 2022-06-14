6426f4b8958632c9aa8cc0f645d654ed9f62c968
andreamunoz176 <72665263+andreamunoz176@users.noreply.github.com>
2022-06-14 14:30:33 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,17 @@
 basic.forever(function () {
     if (tinkercademy.ADKeyboard(ADKeys.A, AnalogPin.P1)) {
-        music.playTone(262, music.beat(BeatFraction.Whole))
+        music.playTone(262, music.beat(BeatFraction.Quarter))
     }
     if (tinkercademy.ADKeyboard(ADKeys.B, AnalogPin.P1)) {
-        music.playTone(294, music.beat(BeatFraction.Whole))
+        music.playTone(294, music.beat(BeatFraction.Quarter))
     }
     if (tinkercademy.ADKeyboard(ADKeys.C, AnalogPin.P1)) {
-        music.playTone(330, music.beat(BeatFraction.Whole))
+        music.playTone(330, music.beat(BeatFraction.Quarter))
     }
     if (tinkercademy.ADKeyboard(ADKeys.D, AnalogPin.P1)) {
-        music.playTone(349, music.beat(BeatFraction.Whole))
+        music.playTone(349, music.beat(BeatFraction.Quarter))
     }
     if (tinkercademy.ADKeyboard(ADKeys.E, AnalogPin.P1)) {
-        music.playTone(392, music.beat(BeatFraction.Whole))
+        music.playTone(392, music.beat(BeatFraction.Quarter))
     }
 })
